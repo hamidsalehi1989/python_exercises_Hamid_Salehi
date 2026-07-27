9b0ba88f9e8173e7e872bbda51cec4dff459d32c
hamidsalehi1989 <hamidsalehiosaka@gmail.com>
2026-07-27 19:50:11 +0330

    Initial commit

---
+++ session-02/tamrin_5.py
@@ -0,0 +1,8 @@
+miles = int(input('Enter the distance(km) : '))
+fixed_fare = 20000
+new_miles= len(miles)*5
+extra = new_miles+5000
+if miles < 2:
+    print('taxi fare : ',fixed_fare)
+elif miles > 2:
+    print('taxi fare : ',)
