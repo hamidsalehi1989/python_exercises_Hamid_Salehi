12401d6fd2486644f04bd13fdb7ac798f9f7c5b8
hamidsalehi1989 <hamidsalehiosaka@gmail.com>
2026-08-02 12:48:25 +0330

    Add files via upload

--- session_02/question_07.py
+++ session_02/question_07.py
@@ -1,7 +1,7 @@
 card_num = input('Enter your card number : ')
-if card_num[::] == 6219 :
+if card_num[0:4] == '6219' and len(card_num) ==16 :
     print('This card has been issued by Bank Mellat')
 else:
     print('Your card number is not valid or has been issued by another bank')
 
-# ask teacher !!!    
+   
